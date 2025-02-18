a71ef1260234415dfdd1cc6cf80928b8e77d3267
psyborg-protocols <aburrows@confluxsolutions.com>
2025-02-18 17:17:50 +0200

    Update auth.js

--- src/auth.js
+++ src/auth.js
@@ -3,7 +3,7 @@ const msalConfig = {
   auth: {
     clientId: "26f834bc-3365-486c-95ff-1a45a24488b5",
     authority: "https://login.microsoftonline.com/b4b6e20e-14bd-4419-bf0a-c7d2c948c513",
-    redirectUri:https://psyborg-protocols.github.io/BWEasySearch/, // Ensure this matches your Azure App's Redirect URI
+    redirectUri: "https://psyborg-protocols.github.io/BWEasySearch/", // matches Azure App's Redirect URI
   }
 };
 
